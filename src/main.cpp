1baca2cef5ffb29be9b08c3a5ac9ea2cf7a338b2
Shane-BR <beastiethepineapple@gmail.com>
2026-06-08 22:04:42 +1200

    Add simple triangle

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,10 @@ private:
     int window_width, window_height;
     std::string title;
     bool running;
+
+    // TODO refactor this out
+    unsigned int VAO, shader_program;
+    
 public:
     bool create(int w, int h, std::string title);
     void main_loop();
@@ -63,6 +67,91 @@ bool Application::create(int w, int h, std::string title)
 
     running = true;
 
+    // TODO Refactor this out
+        float vertices[] = 
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    }; 
+
+    const char* vertex_shader_source = 
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0"
+    ;
+    
+    const char* frag_shader_source =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0"
+    ;
+
+    // Vertex Shader
+    unsigned int vertex_shader;
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+
+    int  success;
+    char info_log[512];
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        SDL_Log("ERROR: Vertex Shader Compilation Failed: \n%s", info_log);
+    }
+
+    // Fragment Shader
+    unsigned int frag_shader;
+    frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag_shader, 1, &frag_shader_source, NULL);
+    glCompileShader(frag_shader);
+
+    glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(frag_shader, 512, NULL, info_log);
+        SDL_Log("ERROR: Fragment Shader Compilation Failed: \n%s", info_log);
+    }
+
+    // Shader program
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, frag_shader);
+    glLinkProgram(shader_program);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(frag_shader);
+
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+        SDL_Log("ERROR: Failed To Link Shader Program: \n%s", info_log);
+    }
+
+    // VAO and VBO
+    unsigned int VBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Atributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     return true;
 }
 
@@ -84,7 +173,7 @@ void Application::main_loop()
                     SDL_Log("Application closed by the user.\n");
                     break;
                 default:
-                    SDL_Log("Unhandled Event.\n");
+                    //SDL_Log("Unhandled Event.\n");
                     break;
             }
         }
@@ -93,8 +182,13 @@ void Application::main_loop()
 
 void Application::render_loop()
 {
+    // Draw
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader_program); // Use shader program from earlier
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     SDL_GL_SwapWindow(window);
 }
